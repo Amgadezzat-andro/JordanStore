65c81231b9b00fb79cf6af98f586a1320c64061f
Amgadezzat-andro <megostar59@gmail.com>
2022-02-11 15:11:30 +0200

    Add Api Resource Part1

---
+++ app/Http/Controllers/Api/CountryController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CityResource;
+use App\Http\Resources\CountryResource;
+use App\Http\Resources\StateResource;
+use App\Models\City;
+use App\Models\Country;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    public function index(){
+
+        return CountryResource::collection(Country::paginate());
+
+    }
+    public function showStates($id){
+
+        $country = Country::find($id);
+        return StateResource::collection($country->states);
+
+    }
+    public function showCities($id){
+        $country = Country::find($id);
+        return CityResource::collection($country->cities);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Get Categories
+Route::get('categories', 'App\HTTP\Controllers\Api\CategoryController@index');
+Route::get('categories/{id}', 'App\HTTP\Controllers\Api\CategoryController@show');
+
+//Get Tags
+Route::get('tags', 'App\HTTP\Controllers\Api\TagController@index');
+Route::get('tags/{id}', 'App\HTTP\Controllers\Api\TagController@show');
+//Get Products
+Route::get('products', 'App\HTTP\Controllers\Api\ProductController@index');
+Route::get('products/{id}', 'App\HTTP\Controllers\Api\ProductController@show');
+
+//General Route
+Route::get('countries', 'App\HTTP\Controllers\Api\CountryController@index');
+Route::get('countries/{id}/states', 'App\HTTP\Controllers\Api\CountryController@showStates');
+Route::get('countries/{id}/cities', 'App\HTTP\Controllers\Api\CountryController@showCities');
+
+
+
+
+
+Route::group(['auth:api'], function () {
+
+    //get full products
+
 });
